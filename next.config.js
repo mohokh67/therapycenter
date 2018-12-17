6a107be59722a1ed22a8dde52a9c1c6f978c47e5
MoHo Khaleqi <habibsandyfan@gmail.com>
2018-12-17 07:39:31 +0000

    [Package] add dotenv

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   // generate static pages
   exportPathMap: function() {
@@ -5,5 +7,23 @@ module.exports = {
       '/': { page: '/' },
       '/about': { page: '/about' }
     };
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret'
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    booking: {
+      lifetimeInDays: process.env.BOOKING_DURATION_IN_DAYS,
+      workingDays: process.env.BOOKING_WORKING_DAYS.split(','),
+      nonWorkingDays: process.env.BOOKING_NON_WORKING_DAYS.split(','),
+      sessionDuration: process.env.BOOKING_SESSION_DURATION,
+      dayBeginsAt: process.env.BOOKING_DAY_BEGINS_AT,
+      dayEndsAt: process.env.BOOKING_DAY_ENDS_AT,
+      dayBreakBeginsAt: process.env.BOOKING_DAY_BREAK_BEGINS_AT,
+      dayBreakEndsAt: process.env.BOOKING_DAY_BREAK_ENDS_AT
+    },
+    applicationName: process.env.APPLICATION_NAME
   }
 };
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,13 @@ import sourceData from '../data';
 import TheMap from '../components/TheMap';
 import Item from '../components/Item';
 
+import getConfig from 'next/config';
+// Only holds serverRuntimeConfig and publicRuntimeConfig from next.config.js nothing else.
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+
+console.log(serverRuntimeConfig.mySecret); // Will only be available on the server side
+console.log(publicRuntimeConfig); // Will be available on both server and client
+
 export default class Index extends Component {
   render() {
     // console.log(sourceData);
